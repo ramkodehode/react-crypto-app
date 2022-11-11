5c0372369065d9b9ae442fac56556d627d301791
ramkodehode <ram.kodehode@gmail.com>
2022-11-11 13:06:02 +0100

    Added api request

--- src/components/Chart/CryptoChart.js
+++ src/components/Chart/CryptoChart.js
@@ -1,16 +1,57 @@
 import Highcharts from 'highcharts/highstock';
 import HighchartsReact from 'highcharts-react-official';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const CryptoChart = ({ coin }) => {
-  const fetchData = () => {
-    axios.get();
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const url =
+    'https://api.coingecko.com/api/v3/coins/bitcoin/market_chart?vs_currency=usd&days=365';
+
+  const getData = async () => {
+    axios
+      .get(url)
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((error) => console.log(error));
   };
 
+  console.log('coins', coins);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   const options = {
+    rangeSelector: {
+      buttons: [
+        {
+          type: 'year',
+          count: 1,
+          text: '3Y',
+          title: 'view',
+        },
+        {
+          type: 'year',
+          count: 1,
+          text: '5Y',
+          title: 'view',
+        },
+        {
+          type: 'year',
+          count: 1,
+          text: 'MAX',
+          title: 'view',
+        },
+      ],
+    },
     title: {
       text: 'Crypto Stock Chart',
     },
+
     series: [
       {
         data: [1, 2, 1, 4, 3, 6, 7, 3, 8, 6, 9],
