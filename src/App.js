f3eb17d0d038876a6ee2364a7039239308c2c302
ramkodehode <ram.kodehode@gmail.com>
2022-11-11 13:05:50 +0100

    Adde Cryptotable

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import CryptoChart from './components/Chart/CryptoChart';
+import CryptoTable from './components/Table/CryptoTable';
 
 function App() {
   return (
     <div className="App">
       <CryptoChart />
+      <CryptoTable />
     </div>
   );
 }
