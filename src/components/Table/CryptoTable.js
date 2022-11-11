3f4ce0d4da0246a75507f4ad314e1bfd276da1ba
ramkodehode <ram.kodehode@gmail.com>
2022-11-11 13:06:17 +0100

    Added CryptoTable components

---
+++ src/components/Table/CryptoTable.js
@@ -0,0 +1,52 @@
+import { CoinList } from '../api';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Form, CoinInput } from './StyledCryptoTable';
+
+const CryptoTable = () => {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
+
+  const getCoinData = () => {
+    axios
+      .get(CoinList('usd'))
+      .then((res) => {
+        setCoins(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  useEffect(() => {
+    getCoinData();
+  }, []);
+
+  console.log('Here is the data', coins);
+
+  {
+  }
+  return (
+    <>
+      <Form>
+        <CoinInput
+          onChange={handleChange}
+          type={'text'}
+          placeholder={'Search'}
+        ></CoinInput>
+      </Form>
+
+      {filteredCoins.map((coin) => {
+        return coin.name;
+      })}
+    </>
+  );
+};
+
+export default CryptoTable;
